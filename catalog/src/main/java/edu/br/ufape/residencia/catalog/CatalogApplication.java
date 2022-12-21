e0a3bcb7cc68d2acb069b42a9e4f0a8aba83b67b
igorvanderlei <igor.vanderlei@gmail.com>
2022-12-21 09:18:25 -0300

    Autenticação

--- catalog/src/main/java/edu/br/ufape/residencia/catalog/CatalogApplication.java
+++ catalog/src/main/java/edu/br/ufape/residencia/catalog/CatalogApplication.java
@@ -2,12 +2,32 @@ package edu.br.ufape.residencia.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages= {"br.edu.ufape.residencia", "edu.br.ufape.residencia.catalog"}
+)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true, securedEnabled = true)
 public class CatalogApplication {
+	private static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogApplication.class, args);
+		applicationContext = SpringApplication.run(CatalogApplication.class, args);
+		checkBeansPresence(
+		          "keycloakJwtAuthenticationConverter", "categoriaController", "catalogApplication");
 	}
 
+
+    private static void checkBeansPresence(String... beans) {
+        for (String beanName : beans) {
+            System.out.println("Is " + beanName + " in ApplicationContext: " + 
+              applicationContext.containsBean(beanName));
+        }
+    }
+	
+	
 }
--- catalog/src/main/java/edu/br/ufape/residencia/catalog/controller/CategoriaController.java
+++ catalog/src/main/java/edu/br/ufape/residencia/catalog/controller/CategoriaController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,7 @@ import edu.br.ufape.residencia.catalog.dto.CategoryRequest;
 import edu.br.ufape.residencia.catalog.dto.CategoryResponse;
 import edu.br.ufape.residencia.catalog.exception.NotFoundException;
 import edu.br.ufape.residencia.catalog.facade.Facade;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @RestController
 public class CategoriaController {
@@ -33,7 +37,16 @@ public class CategoriaController {
 	}
 	
 	@GetMapping(value = "/catalog/categoria")
+	@PreAuthorize("hasAnyAuthority('CADASTRAR_PRODUTO')")
 	public List<CategoryResponse> listAll() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Jwt principal = (Jwt) authentication.getPrincipal();
+		System.out.println(principal.getClaimAsString("email"));
+		System.out.println(principal.getId());
+		System.out.println(authentication.getAuthorities());
+		System.out.println(authentication.getCredentials());
+		System.out.println(authentication.getName());
+		
 		return facade.findAllCategories();
 	}
 	
